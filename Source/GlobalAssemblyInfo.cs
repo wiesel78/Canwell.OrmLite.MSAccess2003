0b16d77b2d7de12fd5075ca73bf3dd86b8fdcf9a
Philip Ullrich <wurst78@gmail.com>
2016-05-27 00:59:06 +0200

    build version: 3.9.71.7

--- Source/GlobalAssemblyInfo.cs
+++ Source/GlobalAssemblyInfo.cs
@@ -6,8 +6,8 @@
 using System.Reflection;
 
 [assembly: AssemblyCompany("canwell - IT Solutions")]
-[assembly: AssemblyVersion("3.9.71.6")]
-[assembly: AssemblyFileVersion("3.9.71.6")]
-[assembly: AssemblyInformationalVersion("3.9.71.6")]
+[assembly: AssemblyVersion("3.9.71.7")]
+[assembly: AssemblyFileVersion("3.9.71.7")]
+[assembly: AssemblyInformationalVersion("3.9.71.7")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 
